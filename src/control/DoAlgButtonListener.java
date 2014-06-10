fcc11c6d6535b6d008d0ca27b85b6aa654754ef4
Fabian <fabian.stroh@gmail.com>
2014-06-10 16:51:12 +0200

    Label zeigt an, was passiert

--- src/control/DoAlgButtonListener.java
+++ src/control/DoAlgButtonListener.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
+import javax.swing.JLabel;
+
 import view.LinePanel;
 import view.PointPanel;
 import view.VisualPoint;
@@ -15,11 +17,13 @@ public class DoAlgButtonListener implements ActionListener {
 	private HamSanAlg hsa;
 	private PointPanel pp;
 	private LinePanel lp;
+	private JLabel l;
 	
-	public DoAlgButtonListener(HamSanAlg h, PointPanel pp, LinePanel lp) {
+	public DoAlgButtonListener(HamSanAlg h, PointPanel pp, LinePanel lp, JLabel label) {
 		this.hsa = h;
 		this.pp = pp;
 		this.lp = lp;
+		this.l = label;
 	}
 	
 	@Override
@@ -29,6 +33,32 @@ public class DoAlgButtonListener implements ActionListener {
 	
 	public void doStuff() {
 		hsa.doAlg();
+		int step = hsa.step;
+		switch (step) {
+		case 1:
+			l.setText("step 1: divided in intervals");
+			break;
+		case 2:
+			l.setText("step 2: found interval with odd intersection property");
+			break;
+		case 3:
+			l.setText("step 3: construted trapeze");
+			break;
+		case 0:
+			l.setText("step 4: removed lines outside the trapeze");
+			break;
+		}
+		if (hsa.getVisualPoints().size()==0) {
+			l.setText("step 0: place points");
+		}
+		if (hsa.done) {
+			if (hsa.validSol(false)){
+				l.setText("found valid solution!");
+			}
+			else {
+				l.setText("found invalid solution");
+			}
+		}
 		lp.followTrapeze();
 		pp.setAddingAllowed(false);
 		List<VisualPoint> vpoints = hsa.getVisualPoints();
