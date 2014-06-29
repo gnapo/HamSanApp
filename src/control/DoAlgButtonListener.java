5b3760e388221c4bc5fb0f2e600c290aac58dc27
Sarah Lutteropp <uwcwa@student.kit.edu>
2014-06-29 18:59:02 +0200

    minor problems with button presses fixed

--- src/control/DoAlgButtonListener.java
+++ src/control/DoAlgButtonListener.java
@@ -20,7 +20,7 @@ public class DoAlgButtonListener implements ActionListener {
 	private LinePanel lp;
 	private JLabel l;
 	private HamSanApplet applet;
-	
+
 	public DoAlgButtonListener(HamSanAlg h, PointPanel pp, LinePanel lp, JLabel label, HamSanApplet hamSanApplet) {
 		this.hsa = h;
 		this.pp = pp;
@@ -28,54 +28,55 @@ public class DoAlgButtonListener implements ActionListener {
 		this.l = label;
 		this.applet = hamSanApplet;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		doStuff();
 	}
-	
+
 	public void doStuff() {
-		hsa.doAlg();
-		int step = hsa.step;
-		switch (step) {
-		case 1:
-			l.setText("step 1: divided in intervals");
-			break;
-		case 2:
-			l.setText("step 2: found interval with odd intersection property");
-			break;
-		case 3:
-			l.setText("step 3: construted trapeze");
-			break;
-		case 4:
-			l.setText("zoomed in on trapeze");
-			lp.followTrapeze();
-		case 0:
-			l.setText("step 4: removed lines outside the trapeze");
-			break;
-		}
-		if (hsa.getVisualPoints().size()==0) {
-			l.setText("step 0: place points");
-		} else {
-			applet.setPlacingEnabled(false);
-		}
-		if (hsa.done) {
-			applet.setStepsEnabled(false);
-			if (hsa.validSol(false)){
-				l.setText("found valid solution!");
+		if (!hsa.done) {
+			hsa.doAlg();
+			int step = hsa.step;
+			switch (step) {
+			case 1:
+				l.setText("step 1: divided in intervals");
+				break;
+			case 2:
+				l.setText("step 2: found interval with odd intersection property");
+				break;
+			case 3:
+				l.setText("step 3: construted trapeze");
+				break;
+			case 4:
+				l.setText("zoomed in on trapeze");
+				lp.followTrapeze();
+			case 0:
+				l.setText("step 4: removed lines outside the trapeze");
+				break;
 			}
-			else {
-				l.setText("found invalid solution");
+			if (hsa.getVisualPoints().size() == 0) {
+				l.setText("step 0: place points");
+			} else {
+				applet.setPlacingEnabled(false);
 			}
+			if (hsa.done) {
+				applet.setStepsEnabled(false);
+				if (hsa.validSol(false)) {
+					l.setText("found valid solution!");
+				} else {
+					l.setText("found invalid solution");
+				}
+			}
+			pp.setAddingAllowed(false);
+			List<VisualPoint> vpoints = hsa.getVisualPoints();
+			pp.setVisualPoints(vpoints);
+			lp.setVisualPoints(vpoints);
+			pp.revalidate();
+			pp.repaint();
+			lp.revalidate();
+			lp.repaint();
 		}
-		pp.setAddingAllowed(false);
-		List<VisualPoint> vpoints = hsa.getVisualPoints();
-		pp.setVisualPoints(vpoints);
-		lp.setVisualPoints(vpoints);
-		pp.revalidate();
-		pp.repaint();
-		lp.revalidate();
-		lp.repaint();
 	}
 
 }
